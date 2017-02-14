c2ef1946dfc208e2a8f791e090b23a433570e279
Grigory Bochkarev <g.bochkaryov@innopolis.ru>
2017-02-14 12:48:19 +0300

    Initial Commit

---
+++ GeneralTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeneralTesting
+//
+//  Created by Grigory Bochkarev on 14.02.17.
+//  Copyright © 2017 GRIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
